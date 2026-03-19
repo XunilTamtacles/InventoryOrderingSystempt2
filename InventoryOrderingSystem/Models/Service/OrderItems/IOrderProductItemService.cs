d4932087aa0467a972190ffaff770a57b13f79fd
XunilTamtacles <bbondocmattlinux@gmail.com>
2026-03-20 02:54:46 +0800

    added an one service

---
+++ InventoryOrderingSystem/Models/Service/OrderItems/IOrderProductItemService.cs
@@ -0,0 +1,22 @@
+﻿using InventoryOrderingSystem.Models.Database;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace InventoryOrderingSystem.Models.Services.OrderProductItems
+{
+    public interface IOrderProductItemService
+    {
+        Task<List<OrderProductItem>> GetAllOrderProductItemsAsync();
+
+        Task<OrderProductItem?> GetOrderProductItemByIdAsync(int id);
+
+        Task CreateOrderProductItemAsync(OrderProductItem item);
+
+        Task UpdateOrderProductItemAsync(OrderProductItem item);
+
+        Task DeleteOrderProductItemAsync(int id);
+       
+     
+
+    }
+}
---
+++ InventoryOrderingSystem/Models/Service/OrderItems/OrderProductItemService.cs
@@ -0,0 +1,41 @@
+﻿using InventoryOrderingSystem.Models.Database;
+using InventoryOrderingSystem.Models.Repositories.OrderProductItems;
+
+namespace InventoryOrderingSystem.Models.Services.OrderProductItems
+{
+    public class OrderProductItemService : IOrderProductItemService
+    {
+        private readonly IOrderProductItemRepository _repository;
+
+        public OrderProductItemService(IOrderProductItemRepository repository)
+        {
+            _repository = repository;
+        }
+
+      
+        public async Task<List<OrderProductItem>> GetAllOrderProductItemsAsync()
+        {
+            return await _repository.GetAllOrderProductItemsAsync();
+        }
+
+        public async Task<OrderProductItem?> GetOrderProductItemByIdAsync(int id)
+        {
+            return await _repository.GetOrderProductItemByIdAsync(id);
+        }
+
+        public async Task CreateOrderProductItemAsync(OrderProductItem item)
+        {
+            await _repository.AddOrderProductItemAsync(item);
+        }
+
+        public async Task UpdateOrderProductItemAsync(OrderProductItem item)
+        {
+            await _repository.UpdateOrderProductItemAsync(item);
+        }
+
+        public async Task DeleteOrderProductItemAsync(int id)
+        {
+            await _repository.DeleteOrderProductItemAsync(id);
+        }
+    }
+}
